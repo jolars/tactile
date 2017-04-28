f5a69f5f4b400c5ccfaaa97bae03694941dead89
Johan Larsson <johanlarsson@outlook.com>
2017-04-28 14:19:16 +0200

    Add some input tests.

--- tests/testthat/test-inputs.R
+++ tests/testthat/test-inputs.R
@@ -1,7 +1,18 @@
 library(latticework)
 
-test_that("We return a list of grobs", {
+test_that("We receive the proper classes", {
   fit <- stats::lm(rnorm(100) ~ runif(100))
-  obj <- lattice::xyplot(fit)
+  expect_error(xyplot(fit), NA)
+  obj <- xyplot(fit, which = 1:6)
+  obj2 <- xyplot(fit, which = 1)
+
   expect_is(obj, "grob")
+  expect_is(obj2, "trellis")
 })
+
+test_that("xyplot.lm throws errors with incorrect input", {
+  fit <- stats::lm(rnorm(100) ~ runif(100))
+  expect_error(xyplot(fit, which = "something"))
+})
+
+

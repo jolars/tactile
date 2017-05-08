acdc3801584629227e6fd154fee28a97f3fde762
Johan Larsson <johanlarsson@outlook.com>
2017-05-08 08:34:00 +0200

    Set strip background to grey.

--- R/latticework_theme.R
+++ R/latticework_theme.R
@@ -27,28 +27,30 @@
 #'
 latticework_theme <- function(font_size = 12,
                               point_size = 8,
-                              name = "pdf",
-                              color = name != "postscript",
+                              color = TRUE,
                               ...) {
-  ll <- lattice::standard.theme(name = name, color = color)
+  ll <- lattice::standard.theme(name = "pdf", color = color)
 
   mod <- list(
     fontsize = list(text = font_size, points = point_size),
-    layout.heights = list(
-      top.padding = 0.1,
-      bottom.padding = 0.1,
-      main = 2,
-      sub = 2,
-      main.key.padding = 0,
-      axis.bottom = 1
-    ),
+    strip.background = list(col = grDevices::grey(9:6/10)),
+    strip.shingle = list(col = grDevices::grey(7:4/10)),
+    strip.text = list(cex = 0.75),
+    layout.heights = list(top.padding = 0.1,
+                          bottom.padding = 0,
+                          main = 2,
+                          sub = 2,
+                          main.key.padding = 0,
+                          key.top = 1.25,
+                          xlab = 1.25),
     layout.widths = list(right.padding = 0.1,
                          left.padding = 0.1,
-                         key.right = 1.5),
+                         key.right = 1.5,
+                         ylab = 1.25),
     par.main.text = list(cex = 1.15),
-    axis.components = list(left = list(tck = 0.5, pad1 = 0.5, pad2 = 0.5),
+    axis.components = list(left = list(tck = 0.5, pad1 = 0.5, pad2 = 0),
                            right = list(tck = 0.5, pad1 = 0.5, pad2 = 0),
-                           bottom = list(tck = 0.5, pad1 = 0.5, pad2 = 0.5),
+                           bottom = list(tck = 0.5, pad1 = 0.5, pad2 = 0),
                            top = list(tck = 0.5, pad1 = 0.5, pad2 = 0))
   )
   utils::modifyList(utils::modifyList(ll, mod), list(...))

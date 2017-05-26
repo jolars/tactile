1f2571e9d16af4d5847197a63360cdfbd2dd4cf9
Johan Larsson <johanlarsson@outlook.com>
2017-05-26 21:09:22 +0200

    Add some utility functions.

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,6 @@
 # Wrapper around grid.arrange()
 grid_wrap <- function(x, layout = NULL) {
   x <- x[!vapply(x, is.null, FUN.VALUE = logical(1))]
-
   if (length(x) == 1) {
     update(x[[1]])
   } else {
@@ -26,7 +25,6 @@ rescale <- function(x, new_min = 0, new_max = 1) {
   (x - old_min)/(old_max - old_min) * (new_max - new_min) + new_min
 }
 
-
 # suppress plot, return object
 dont_plot <- function(x) {
   tmp <- tempfile()
@@ -35,4 +33,45 @@ dont_plot <- function(x) {
   grDevices::dev.off()
   unlink(tmp)
   invisible(p)
+}
+
+updateList <- function(x, val) {
+  if (is.null(x))
+    x <- list()
+  if (!is.list(val))
+    tryCatch(val <- as.list(val))
+  if (!is.list(x))
+    tryCatch(x <- as.list(x))
+  modifyList(x, val)
+}
+
+# Retrieve function even if it is only named
+get_fun <- function(fun) {
+  if (is.function(fun))
+    fun
+  else if (is.character(fun))
+    get(fun)
+  else
+    eval(fun)
+}
+
+# Convert a z ~ x * y formula to y ~ x
+xyz_to_xy <- function(form) {
+  new_form <- paste(form$right.y.name, "~", form$right.x.name)
+  if (!is.null(form$condition))
+    new_form <- paste(new_form, "|", paste(names(form$condition), sep = " + "))
+  as.formula(new_form)
+}
+
+# Require packages that are only listed in Suggests
+require_pkg <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE))
+    stop(paste("Package ",  pkg, " is needed for this function to work. Please install it."),
+         call. = FALSE)
+}
+
+# Borrowed from lattice
+needAutoKey <- function(auto.key, groups = NULL) {
+  (!is.null(groups) && (isTRUE(auto.key) || is.list(auto.key))) ||
+     (is.list(auto.key) && !is.null(auto.key$text))
 }

11c28a9f7875224ab843eb6d774f6f70a0ff9f4f
Johan Larsson <johanlarsson@outlook.com>
2017-06-12 08:47:06 +0200

    Add Arima tests.

---
+++ tests/testthat/test-methods.R
@@ -0,0 +1,12 @@
+library(tactile)
+
+test_that("xyplot.Arima", {
+  fit <- arima(lh, c(0, 0, 1))
+  expect_error(xyplot(fit, which = 1:5))
+  expect_error(xyplot(fit, main = letters[1:5]))
+})
+
+test_that("xyplot.forecast", {
+  fit <- forecast::forecast(arima(lh, c(0, 0, 1)))
+})
+

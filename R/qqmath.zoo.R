cf688c35146e6bd1184d6d29053ce26a0246bd2e
Johan Larsson <johanlarsson@outlook.com>
2017-06-12 07:29:04 +0200

    Make example conditional on zoo.

--- R/qqmath.zoo.R
+++ R/qqmath.zoo.R
@@ -1,28 +1,33 @@
-#' Q-Q Diagram for Zoo Time Series
+#' Q-Q Plots for Zoo Objects
 #'
 #' Draw quantile-Quantile plots of a sample against a theoretical distribution,
 #' possibly conditioned on other variables.
 #'
 #' @param x A `zoo` object
 #' @param data Ignored
-#' @param ref Plots a reference line via [lattice::panel.qqmathline]
-#' @param ci Plots confidence levels via [panel.qqmathci]
-#' @param \dots Parameters to pass on to [lattice::qqmath()].
+#' @param xlab X axis label
+#' @param ylab Y axis label
+#' @param ref Plot a reference line via [lattice::panel.qqmathline()].
+#' @param ci Plot confidence levels via [panel.qqmathci()].
+#' @param ... Parameters to pass on to [lattice::qqmath()].
 #'
 #' @return Plots and returns a `trellis` object.
-#' @author Original by Deepayan Sarkar. Method by Johan Larsson.
+#' @author Original by Deepayan Sarkar.
 #' @seealso [lattice::qqmath()], [zoo::zoo()], [lattice::panel.qqmathline()].
 #' @export
 #'
 #' @examples
-#' qqmath(zoo::zoo(lh))
-qqmath.zoo <- function(x,
-                       data = NULL,
-                       xlab = "Theoretical quantiles",
-                       ylab = "Sample quantiles",
-                       ref = TRUE,
-                       ci = TRUE,
-                       ...) {
+#' if (require(zoo))
+#'   qqmath(zoo(lh))
+qqmath.zoo <- function(
+  x,
+  data = NULL,
+  xlab = "Theoretical quantiles",
+  ylab = "Sample quantiles",
+  ref = TRUE,
+  ci = TRUE,
+  ...
+) {
   do.call(qqmath, updateList(list(
     x = ~ zoo::coredata(x),
     xlab = xlab,
